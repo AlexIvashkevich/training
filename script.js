0f00198d343c7b8d67e4e6b7485648d853b2db9e
AlexIvashkevich <58821909+AlexIvashkevich@users.noreply.github.com>
2020-05-16 17:07:08 +0300

    learn commands

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log('hi')
 console.log('2branch')
-console.log('newbranch')
+console.log('newbranch') 
+// learn commands git add . git status git checkout git commit -m git branch git merge (gitflow) git push git pull
